79ccc9eca651e89f682a49ef037c221106d317cd
Michael <kronenberg1991@gmail.com>
2017-06-14 23:19:36 +0300

    test

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
   if (req.user && req.user.email === 'kronenberg1991@gmail.com') {
     res.send({message: `${req.user.email} what i can do for you master`});
   } else {
-    res.boom.unauthorized('Burn in hell u little motherfucker!');
+    res.boom.unauthorized('Forbidden! Good luck!');
   }
 });
 
